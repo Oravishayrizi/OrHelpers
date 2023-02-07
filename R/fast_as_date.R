872b00ba7799fbb04cdd5ff3b7d351bc3d2efba7
Or Avishay-Rizi <30078665+Oravishayrizi@users.noreply.github.com>
2023-02-07 15:38:10 +0200

    improved fast_as_date

--- R/fast_as_date.R
+++ R/fast_as_date.R
@@ -1,28 +1,27 @@
-#' Fast change to date class
+#' Fast change to Date class
 #'
-#' This function is a wrapper function of the `lubridate::fast_strptime` function. It coverts a character or factor vector into a vector of dates.
+#' This function coverts a character or factor vector into a vector of dates.
 #'
 #' @param x a vector to be converted into date.
-#' @param format the format of the dates. The default format is "2020-12-30", different format can be used but should be explicitly defined. For other formats of dates look at: <https://www.stat.berkeley.edu/~s133/dates.html>
-#' @return A vector of Dates in format of `YYYY-MM-DD`
+#' @return A vector of Dates
 #' @export
-#' @seealso see [`lubridate::fast_strptime`] help for more information
+#' @references Based on <https://towardsdatascience.com/tricks-in-r-to-boost-your-productivity-8c977242c69c>
 #' @examples
 #' fast_as_date(c("2020-10-15","2020-12-03"))
 #' fast_as_date(c("10/15/20","12/03/20"),format='%m/%d/%y')
-#' fast_as_date(c("October 15, 2020","December 03, 2020"),format='%B %d, %Y')
-fast_as_date<-function(x,format=NULL){
-    if (is.null(format)) {
-        format<-"%Y-%m-%d"
-    }
-    if(!is.vector(x)){
-        stop('x should be a vector')
-    }
-
+fast_as_date<-function(x,...){
     if (is.factor(x)) {
-        x<-as.character(x)
+        x <- as.character(x)
     }
-    if (is.character(x))
-        x<-as.Date(lubridate::fast_strptime(x,format))
+    if (is.character(x)) {
+        if (anyDuplicated(x)) {
+            ux <- unique(x)
+            idx <- match(x, ux)
+            y <- as.Date.character(ux,...)
+            x <- y[idx]
+        } else{
+            x <- as.Date.character(x,...)
+        }
+    }
     return(x)
 }
